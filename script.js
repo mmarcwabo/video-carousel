5469d73415a65ea06088e53b9d87c744740052d1
Marcellin Wabo <marcellin@wabo.work>
2022-05-20 00:59:07 +0100

    Update video carousel

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ $(document).ready(function () {
         $slideText = $root.find(".video-carousel-text"),
         isAnimating = false,
         transition = { "left": slideLeftTransition, "right": slideRightTransition },
+        video = $('video'),
         slideTimer, myPlayer, video_counter = 0;
 
     setClasses();
@@ -97,4 +98,30 @@ $(document).ready(function () {
         }
         return false;
     }
+
+    function playVideo() {
+        video.play();
+    }
+    function pauseVideo() {
+        video.pause();        
+    }
+});
+
+const video = document.getElementById("video");
+const circlePlayButton = document.getElementById("circle-play-b");
+
+function togglePlay() {
+	if (video.paused || video.ended) {
+		video.play();
+	} else {
+		video.pause();
+	}
+}
+
+circlePlayButton.addEventListener("click", togglePlay);
+video.addEventListener("playing", function () {
+	circlePlayButton.style.opacity = 0;
+});
+video.addEventListener("pause", function () {
+	circlePlayButton.style.opacity = 1;
 });
